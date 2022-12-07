5f2bad9f6342c0dbe85db2cf552a3d1a303ad0bd
maxzou890j3 <95652615+maxzou890j3@users.noreply.github.com>
2022-12-07 11:56:07 -0600

    FInal version of Shiny App

---
+++ server.R
@@ -0,0 +1,34 @@
+library(shiny)
+library(shinythemes)
+library(tidyverse)
+library(ggthemes)
+library(ggplot2)
+
+df = read_csv("full_data.csv")
+kws = list('roast_beef', 'customer_service', 'freaky_fast', 'staff_friendly',
+           'meat_cheese', 'hot_sauce', 'fresh_bread', 'lettuce_tomato',
+           'cheese_steak', 'hot_pepper', 'meatball_sub', 'drive_thru',
+           'great_service', 'italian_sub', 'online_order', '10_minute',
+           '15_minute', '20_minute', '30_minute', 'bag_chip', 'one_star',
+           'wont_back', 'turkey_bacon', 'big_kahuna', 'order_wrong')
+histogram <- function(df) {
+  ggplot(df, aes(x=compound)) +
+    geom_histogram(color="black", fill="white") +
+    ggtitle("Distribution of Sentiment") 
+}
+
+server <- function(input, output) {
+  
+  
+  output$text <- renderText({
+    paste("Median Sentiment (between -1 and 1): ",
+          median(df$compound[df$state %in% input$State & df[colnames(df) == input$Key_Words] == 1]),
+          sep = "")
+    
+  }) 
+  
+  output$hist <- renderPlot({
+    histogram(df[df$state %in% input$State & df[colnames(df) == input$Key_Words] == 1, ])
+  })
+  #output$table <- renderTable(df[df$state %in% input$State & df[colnames(df) == input$Key_Words] == 1, ])
+}
---
+++ ui.R
@@ -0,0 +1,31 @@
+library(shiny)
+library(shinythemes)
+library(tidyverse)
+library(ggthemes)
+library(ggplot2)
+
+df = read_csv("full_data.csv")
+kws = list('roast_beef', 'customer_service', 'freaky_fast', 'staff_friendly',
+           'meat_cheese', 'hot_sauce', 'fresh_bread', 'lettuce_tomato',
+           'cheese_steak', 'hot_pepper', 'meatball_sub', 'drive_thru',
+           'great_service', 'italian_sub', 'online_order', '10_minute',
+           '15_minute', '20_minute', '30_minute', 'bag_chip', 'one_star',
+           'wont_back', 'turkey_bacon', 'big_kahuna', 'order_wrong')
+histogram <- function(df) {
+  ggplot(df, aes(x=compound)) +
+    geom_histogram(color="black", fill="white") +
+    ggtitle("Distribution of Sentiment") 
+}
+
+ui <- fluidPage(
+  tags$div(
+    HTML("<h1 style='color:black;text-align:center;'>Yelp Review Sentiment Analysis of Sandwich Shops</h1>")
+  ),  
+  
+  selectInput("State", label = "State", choices = unique(df$state), multiple = TRUE, selected = unique(df$state)),
+  selectInput("Key_Words", label = "Kw", choices = kws, multiple = FALSE),
+  
+  textOutput("text"),
+  plotOutput("hist"),
+  #tableOutput('table')
+)
