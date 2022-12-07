29c37242df2b572f50f820e7b9eec8473040a6f3
maxzou890j3 <95652615+maxzou890j3@users.noreply.github.com>
2022-12-07 17:17:04 -0600

    Update Shiny App

--- server.R
+++ server.R
@@ -13,7 +13,9 @@ kws = list('roast_beef', 'customer_service', 'freaky_fast', 'staff_friendly',
            'wont_back', 'turkey_bacon', 'big_kahuna', 'order_wrong')
 histogram <- function(df) {
   ggplot(df, aes(x=compound)) +
-    geom_histogram(color="black", fill="white") +
+    geom_histogram(color="darkblue", fill="lightblue") +
+    geom_vline(aes(xintercept=0),color="blue", linetype="dashed", size=1)+
+    labs(x="Sentiment Score") +
     ggtitle("Distribution of Sentiment") 
 }
 
--- ui.R
+++ ui.R
@@ -1,8 +1,10 @@
 library(shiny)
 library(shinythemes)
+library(bslib)
 library(tidyverse)
 library(ggthemes)
 library(ggplot2)
+
 
 df = read_csv("full_data.csv")
 kws = list('roast_beef', 'customer_service', 'freaky_fast', 'staff_friendly',
@@ -13,21 +15,27 @@ kws = list('roast_beef', 'customer_service', 'freaky_fast', 'staff_friendly',
            'wont_back', 'turkey_bacon', 'big_kahuna', 'order_wrong')
 histogram <- function(df) {
   ggplot(df, aes(x=compound)) +
-    geom_histogram(color="black", fill="blue") +
+    geom_histogram(color="darkblue", fill="lightblue") +
+    labs(x="Sentiment Score") +
     ggtitle("Distribution of Sentiment") 
 }
 
 ui <- fluidPage(
+  theme = bslib::bs_theme(bootswatch = "superhero"),
   tags$div(
-    HTML("<h1 style='color:black;text-align:center;'>Yelp Review Sentiment Analysis of Sandwich Shops</h1>")
+    HTML("<h1 style='color:white;text-align:center;'>Yelp Review Sentiment Analysis of Sandwich Shops</h1>")
   ),  
-  
-  selectInput("State", label = "State", choices = unique(df$state), multiple = TRUE, selected = unique(df$state)),
-  selectInput("Brand", label = "Brand", choices = unique(df$brand), multiple = TRUE, selected = unique(df$brand)),
-  selectInput("Key_Words", label = "Kw", choices = kws, multiple = FALSE),
-  
-  textOutput("text"),
-  plotOutput("hist"),
+  sidebarLayout(
+    sidebarPanel(
+      selectInput("State", label = "State", choices = unique(df$state), multiple = TRUE, selected = c('PA', 'CA', 'TN', 'FL', 'NV', 'AZ')),
+      selectInput("Brand", label = "Brand", choices = unique(df$brand), multiple = TRUE, selected = c("Subway", "Firehouse Subs", "Jersey Mike's Subs", "Jimmy John's" )),
+      selectInput("Key_Words", label = "Key Words", choices = kws, multiple = FALSE)
+    ),
+    mainPanel(
+      textOutput("text"),
+      plotOutput("hist")
+    )
+  ),
   "Thank you for using our app, if you have any questions, please contact: zou47@wisc.edu"
   #tableOutput('table')
 )
